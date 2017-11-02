cad3765c9bab67398a6c95c156e4081f700c9c4d
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-02 08:46:15 +0000

    new deployment script

--- release.sh
+++ release.sh
@@ -9,3 +9,11 @@ git push origin v${NUMBER}
 python setup.py clean --all
 python setup.py sdist bdist_wheel upload
 python setup.py clean --all
+
+if [[ -x /dls_sw/apps/jira/deploy ]]; then
+  read -p "Do you want to deploy this @DLS for all users? " -n 1 -r
+  echo # move to new line
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    /dls_sw/apps/jira/deploy ${NUMBER}
+  fi
+fi
