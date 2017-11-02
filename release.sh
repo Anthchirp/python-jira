a8f6d6b668ac23bee527a528e3e0d0a4d676e9ba
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-02 09:54:49 +0000

    use twine for uploading

--- release.sh
+++ release.sh
@@ -3,11 +3,15 @@ set -e
 
 python -c "import sys; assert sys.hexversion >= 0x02070000, 'python too old'"
 python -c "import wheel" || python -m pip install wheel
+python -c "import twine" || python -m pip install twine
+python setup.py clean --all
+python setup.py sdist bdist_wheel
+
+python -m twine upload dist/*
+
 export NUMBER=$(python -c "import jiradls; print jiradls.__version__")
 git tag -a v${NUMBER} -m v${NUMBER}
 git push origin v${NUMBER}
-python setup.py clean --all
-python setup.py sdist bdist_wheel upload
 python setup.py clean --all
 
 if [[ -x /dls_sw/apps/jira/deploy ]]; then
