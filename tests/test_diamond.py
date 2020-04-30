bf513b3cc621809492766347179a1bb7557493ad
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-04-30 09:23:21 +0100

    apply black to entire repository

--- tests/test_diamond.py
+++ tests/test_diamond.py
@@ -2,49 +2,107 @@ from __future__ import absolute_import, division, print_function
 
 import jiradls.diamond
 
+
 def test_issue_number_detection():
-  # Numbers below 1000 are rejected
-  assert jiradls.diamond.issue_number('1') == False
-  assert jiradls.diamond.issue_number('12') == False
-  assert jiradls.diamond.issue_number('123') == False
+    # Numbers below 1000 are rejected
+    assert jiradls.diamond.issue_number("1") == False
+    assert jiradls.diamond.issue_number("12") == False
+    assert jiradls.diamond.issue_number("123") == False
 
-  # Numbers above 1000 are interpreted as SCI project
-  assert jiradls.diamond.issue_number('1234') == 'SCI-1234'
+    # Numbers above 1000 are interpreted as SCI project
+    assert jiradls.diamond.issue_number("1234") == "SCI-1234"
 
-  # Non-numerical strings are rejected
-  assert jiradls.diamond.issue_number('1234s') == False
-  assert jiradls.diamond.issue_number('1s') == False
+    # Non-numerical strings are rejected
+    assert jiradls.diamond.issue_number("1234s") == False
+    assert jiradls.diamond.issue_number("1s") == False
 
-  # Accept issue names
-  assert jiradls.diamond.issue_number('SCRATCH-1234') == 'SCRATCH-1234'
-  assert jiradls.diamond.issue_number('THING-1') == 'THING-1'
-  # Accept and change case
-  assert jiradls.diamond.issue_number('i04_1-7') == 'I04_1-7'
+    # Accept issue names
+    assert jiradls.diamond.issue_number("SCRATCH-1234") == "SCRATCH-1234"
+    assert jiradls.diamond.issue_number("THING-1") == "THING-1"
+    # Accept and change case
+    assert jiradls.diamond.issue_number("i04_1-7") == "I04_1-7"
 
-  # Reject invalid issue names
-  assert jiradls.diamond.issue_number('SCRATCH-1234x') == False
-  assert jiradls.diamond.issue_number('abc-def-1234') == False
-  assert jiradls.diamond.issue_number('banana') == False
+    # Reject invalid issue names
+    assert jiradls.diamond.issue_number("SCRATCH-1234x") == False
+    assert jiradls.diamond.issue_number("abc-def-1234") == False
+    assert jiradls.diamond.issue_number("banana") == False
 
-  # Other things
-  assert jiradls.diamond.issue_number('') == False
-  assert jiradls.diamond.issue_number(None) == False
+    # Other things
+    assert jiradls.diamond.issue_number("") == False
+    assert jiradls.diamond.issue_number(None) == False
+
 
 def test_fixversion_filtering():
-  candidate_versions = ['Run 1 (2019)', 'Shutdown 1 (2019)', 'Run 2 (2019)', 'Shutdown 2 (2019)', 'Diamond-DAWN 1.0', 'Diamond-Dawn 1.1', 'gda-8.30', 'gda-8.32', 'gda-8.34', 'Dawn 1.5', 'gda-8.36', 'gda-8.38', 'gda-8.40', 'gda-8.42', 'gda-8.44', 'DAWN 1.8', 'gda-8.46', 'gda-8.48', 'gda-8.50', 'gda-8.52', 'gda-9.0', 'gda-9.1', 'Dawn-1.9', 'Dawn-1.10', 'Dawn 2.0', '1.0', 'Run 5 (2017)', 'Shutdown 5 (2017)', 'Run 1 (2018)', 'Shutdown 1 (2018)', 'Run 2 (2018)', 'Shutdown 2 (2018)', 'Run 3 (2018)', 'Shutdown 3 (2018)', 'Run 4 (2018)', 'Shutdown 4 (2018)', 'Run 5 (2018)', 'Shutdown 5 (2018)']
-  filtered_versions = jiradls.diamond.filter_versions(candidate_versions, run=2, year=2018)
-  assert filtered_versions == [ 'Run 1 (2019)', 'Shutdown 1 (2019)', 'Run 2 (2019)', 'Shutdown 2 (2019)', 'Run 2 (2018)', 'Shutdown 2 (2018)', 'Run 3 (2018)', 'Shutdown 3 (2018)', 'Run 4 (2018)', 'Shutdown 4 (2018)', 'Run 5 (2018)', 'Shutdown 5 (2018)' ]
+    candidate_versions = [
+        "Run 1 (2019)",
+        "Shutdown 1 (2019)",
+        "Run 2 (2019)",
+        "Shutdown 2 (2019)",
+        "Diamond-DAWN 1.0",
+        "Diamond-Dawn 1.1",
+        "gda-8.30",
+        "gda-8.32",
+        "gda-8.34",
+        "Dawn 1.5",
+        "gda-8.36",
+        "gda-8.38",
+        "gda-8.40",
+        "gda-8.42",
+        "gda-8.44",
+        "DAWN 1.8",
+        "gda-8.46",
+        "gda-8.48",
+        "gda-8.50",
+        "gda-8.52",
+        "gda-9.0",
+        "gda-9.1",
+        "Dawn-1.9",
+        "Dawn-1.10",
+        "Dawn 2.0",
+        "1.0",
+        "Run 5 (2017)",
+        "Shutdown 5 (2017)",
+        "Run 1 (2018)",
+        "Shutdown 1 (2018)",
+        "Run 2 (2018)",
+        "Shutdown 2 (2018)",
+        "Run 3 (2018)",
+        "Shutdown 3 (2018)",
+        "Run 4 (2018)",
+        "Shutdown 4 (2018)",
+        "Run 5 (2018)",
+        "Shutdown 5 (2018)",
+    ]
+    filtered_versions = jiradls.diamond.filter_versions(
+        candidate_versions, run=2, year=2018
+    )
+    assert filtered_versions == [
+        "Run 1 (2019)",
+        "Shutdown 1 (2019)",
+        "Run 2 (2019)",
+        "Shutdown 2 (2019)",
+        "Run 2 (2018)",
+        "Shutdown 2 (2018)",
+        "Run 3 (2018)",
+        "Shutdown 3 (2018)",
+        "Run 4 (2018)",
+        "Shutdown 4 (2018)",
+        "Run 5 (2018)",
+        "Shutdown 5 (2018)",
+    ]
 
-  filtered_versions = jiradls.diamond.filter_versions(candidate_versions, run=5, year=2017, return_map=True)
-  assert filtered_versions == {
-    'run1': 'Run 1 (2018)',
-    'run2': 'Run 2 (2018)',
-    'run3': 'Run 3 (2018)',
-    'run4': 'Run 4 (2018)',
-    'run5': 'Run 5 (2017)',
-    'shutdown1': 'Shutdown 1 (2018)',
-    'shutdown2': 'Shutdown 2 (2018)',
-    'shutdown3': 'Shutdown 3 (2018)',
-    'shutdown4': 'Shutdown 4 (2018)',
-    'shutdown5': 'Shutdown 5 (2017)',
-  }
+    filtered_versions = jiradls.diamond.filter_versions(
+        candidate_versions, run=5, year=2017, return_map=True
+    )
+    assert filtered_versions == {
+        "run1": "Run 1 (2018)",
+        "run2": "Run 2 (2018)",
+        "run3": "Run 3 (2018)",
+        "run4": "Run 4 (2018)",
+        "run5": "Run 5 (2017)",
+        "shutdown1": "Shutdown 1 (2018)",
+        "shutdown2": "Shutdown 2 (2018)",
+        "shutdown3": "Shutdown 3 (2018)",
+        "shutdown4": "Shutdown 4 (2018)",
+        "shutdown5": "Shutdown 5 (2017)",
+    }
--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,87 +1,90 @@
 from __future__ import absolute_import, division, print_function
 import jiradls.workflow
 
+
 def test_dls_workflow_status_dictionary():
-  assert jiradls.workflow.status_name[4] == "analysis"
-  assert jiradls.workflow.status_id["analysis"] == 4
+    assert jiradls.workflow.status_name[4] == "analysis"
+    assert jiradls.workflow.status_id["analysis"] == 4
+
 
 def test_dls_workflow_known_transitions():
-  assert 3 in jiradls.workflow.transitions[1]
+    assert 3 in jiradls.workflow.transitions[1]
+
 
 def test_route_finding_single_target():
-  # Open -> Open
-  routes = jiradls.workflow.route_workflow(1, 1)
-  assert routes == [ [1] ]
+    # Open -> Open
+    routes = jiradls.workflow.route_workflow(1, 1)
+    assert routes == [[1]]
 
-  # Open -> Active
-  routes = jiradls.workflow.route_workflow(1, 3)
-  assert routes == [ [1, 3],
-                     [1, 2, 3],
-                     [1, 7, 3],
-                     [1, 2, 7, 3] ]
+    # Open -> Active
+    routes = jiradls.workflow.route_workflow(1, 3)
+    assert routes == [[1, 3], [1, 2, 3], [1, 7, 3], [1, 2, 7, 3]]
 
-# import pprint
-# pprint.pprint(routes)
+    # import pprint
+    # pprint.pprint(routes)
 
-  # Wait deploy -> In Progress
-  routes = jiradls.workflow.route_workflow(9, 10)
-  assert routes == [ [9, 8, 1, 10] ]
+    # Wait deploy -> In Progress
+    routes = jiradls.workflow.route_workflow(9, 10)
+    assert routes == [[9, 8, 1, 10]]
 
-  # Open -> Resolved
-  routes = jiradls.workflow.route_workflow(1, 7)
-  assert routes[0] == [1, 7]
-  assert len(routes) > 20
+    # Open -> Resolved
+    routes = jiradls.workflow.route_workflow(1, 7)
+    assert routes[0] == [1, 7]
+    assert len(routes) > 20
 
-  # Open -> Resolved, avoiding Resolved
-  routes = jiradls.workflow.route_workflow(1, 7, {7})
-  assert routes == [ ]
+    # Open -> Resolved, avoiding Resolved
+    routes = jiradls.workflow.route_workflow(1, 7, {7})
+    assert routes == []
 
-  # Validation -> Open, not via Analysis or Development
-  routes = jiradls.workflow.route_workflow(6, 1, {4, 5})
-  assert routes == [ [6, 3, 2, 1],
-                     [6, 7, 8, 1],
-                     [6, 3, 7, 8, 1],
-                     [6, 7, 3, 2, 1],
-                     [6, 3, 2, 7, 8, 1] ]
+    # Validation -> Open, not via Analysis or Development
+    routes = jiradls.workflow.route_workflow(6, 1, {4, 5})
+    assert routes == [
+        [6, 3, 2, 1],
+        [6, 7, 8, 1],
+        [6, 3, 7, 8, 1],
+        [6, 7, 3, 2, 1],
+        [6, 3, 2, 7, 8, 1],
+    ]
+
 
 def test_route_finding_multi_target():
-  # Open -> Open or In Progress
-  routes = jiradls.workflow.route_workflow(1, [1, 10])
-  assert routes == [ [1] ]
+    # Open -> Open or In Progress
+    routes = jiradls.workflow.route_workflow(1, [1, 10])
+    assert routes == [[1]]
 
-  # Open -> Active or In Progress
-  routes = jiradls.workflow.route_workflow(1, [3, 10])
-  assert routes == [ [1, 3],
-                     [1, 10],
-                     [1, 2, 3],
-                     [1, 7, 3],
-                     [1, 2, 7, 3] ]
+    # Open -> Active or In Progress
+    routes = jiradls.workflow.route_workflow(1, [3, 10])
+    assert routes == [[1, 3], [1, 10], [1, 2, 3], [1, 7, 3], [1, 2, 7, 3]]
 
-# import pprint
-# pprint.pprint(routes)
+    # import pprint
+    # pprint.pprint(routes)
 
-  # Wait deploy -> Active or In Progress
-  routes = jiradls.workflow.route_workflow(9, [3, 10])
-  assert routes == [ [9, 8, 1, 3],
-                     [9, 8, 1, 10],
-                     [9, 8, 1, 2, 3],
-                     [9, 8, 1, 7, 3],
-                     [9, 8, 1, 2, 7, 3] ]
+    # Wait deploy -> Active or In Progress
+    routes = jiradls.workflow.route_workflow(9, [3, 10])
+    assert routes == [
+        [9, 8, 1, 3],
+        [9, 8, 1, 10],
+        [9, 8, 1, 2, 3],
+        [9, 8, 1, 7, 3],
+        [9, 8, 1, 2, 7, 3],
+    ]
 
-  # Open -> Resolved or Closed
-  routes = jiradls.workflow.route_workflow(1, [7, 8])
-  assert routes[0] == [1, 7]
-  assert [1, 10, 8] in routes
-  assert len(routes) > 20
+    # Open -> Resolved or Closed
+    routes = jiradls.workflow.route_workflow(1, [7, 8])
+    assert routes[0] == [1, 7]
+    assert [1, 10, 8] in routes
+    assert len(routes) > 20
 
-  # Open -> Resolved or Closed, avoiding Resolved
-  routes = jiradls.workflow.route_workflow(1, [7, 8], {7})
-  assert routes == [ [1, 8], [1, 10, 8], [1, 10, 9, 8] ]
+    # Open -> Resolved or Closed, avoiding Resolved
+    routes = jiradls.workflow.route_workflow(1, [7, 8], {7})
+    assert routes == [[1, 8], [1, 10, 8], [1, 10, 9, 8]]
 
-  # Open -> Validation or Wait Deploy, avoiding Analysis/Development
-  routes = jiradls.workflow.route_workflow(1, [6, 9], {4, 5})
-  assert routes == [ [1, 3, 6],
-                     [1, 10, 9],
-                     [1, 2, 3, 6],
-                     [1, 7, 3, 6],
-                     [1, 2, 7, 3, 6]]
+    # Open -> Validation or Wait Deploy, avoiding Analysis/Development
+    routes = jiradls.workflow.route_workflow(1, [6, 9], {4, 5})
+    assert routes == [
+        [1, 3, 6],
+        [1, 10, 9],
+        [1, 2, 3, 6],
+        [1, 7, 3, 6],
+        [1, 2, 7, 3, 6],
+    ]
